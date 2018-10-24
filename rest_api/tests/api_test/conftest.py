1aa1d739cc7402faf75ced76f575ad752c62d8c4
adityasingh177 <adityasingh27@hotmail.com>
2018-10-24 15:56:21 +0530

    Update conftest.py

--- rest_api/tests/api_test/conftest.py
+++ rest_api/tests/api_test/conftest.py
@@ -1,4 +1,3 @@
-
 # Copyright 2018 Intel Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ------------------------------------------------------------------------------
+  
 import pytest
 import sys
 import platform
@@ -21,6 +21,7 @@ import logging
 import urllib
 import json
 import os
+import time
 
 from sawtooth_signing import create_context
 from sawtooth_signing import CryptoFactory
@@ -44,7 +45,7 @@ from utils import get_batches,  get_transactions, get_state_address, post_batch,
                   get_state_list , _delete_genesis , _start_validator, \
                   _stop_validator , _create_genesis , _get_client_address, \
                   _stop_settings_tp, _start_settings_tp, _get_client_address, batch_count, transaction_count,\
-                  get_batch_statuses
+                  get_batch_statuses, state_count
 
 from payload import get_signer, create_intkey_transaction , create_batch
                   
@@ -54,7 +55,115 @@ LOGGER.setLevel(logging.INFO)
 
 
 LIMIT = 100
-BATCH_SIZE = 15
+BATCH_SIZE = 1
+
+def _create_transaction():
+    txns = [create_intkey_transaction("set", [] , 50 , signer) for i in range(BATCH_SIZE)]
+    return txns
+    
+
+def _create_batch():
+    batches = [create_batch([txn], signer) for txn in txns]
+    return batches
+
+@pytest.fixture(scope="session")
+def setup(request):
+    """Setup method for posting batches and returning the 
+       response
+    """
+    LOGGER.info("Starting setup method for test cases")
+    data = {}
+    signer = get_signer()
+    expected_trxns  = {}
+    expected_batches = []
+    transaction_list = []
+    initial_state_length = state_count()
+    initial_batch_length = batch_count()
+    initial_transaction_length = transaction_count()
+    address = _get_client_address()
+
+    LOGGER.info("Creating intkey transactions with set operations")
+    
+    txns = [create_intkey_transaction("set", [] , 50 , signer) for i in range(BATCH_SIZE)]
+
+    for txn in txns:
+        dict = MessageToDict(
+                txn,
+                including_default_value_fields=True,
+                preserving_proto_field_name=True)
+        
+        if 'trxn_id' not in expected_trxns:
+            expected_trxns['trxn_id'] = []
+        if 'payload' not in expected_trxns:
+            expected_trxns['payload'] =[]
+                
+        expected_trxns['trxn_id'].append(dict['header_signature'])
+        expected_trxns['payload'].append(dict['payload'])
+                        
+    LOGGER.info("Creating batches for transactions 1trn/batch")
+
+    batches = [create_batch([txn], signer) for txn in txns]
+
+    for batch in batches:
+        dict = MessageToDict(
+                batch,
+                including_default_value_fields=True,
+                preserving_proto_field_name=True)
+
+        batch_id = dict['header_signature']
+        expected_batches.append(batch_id)
+    
+    length_batches = len(expected_batches)
+    length_transactions = len(expected_trxns)
+        
+    data['expected_batch_length'] = initial_batch_length + length_batches
+    data['expected_trn_length'] = initial_transaction_length + length_transactions
+    data['expected_txns'] = expected_trxns['trxn_id'][::-1]
+    data['payload'] = expected_trxns['payload'][::-1]
+    data['expected_batches'] = expected_batches[::-1]
+    data['signer_key'] = signer.get_public_key().as_hex()
+    
+    post_batch_list = [BatchList(batches=[batch]).SerializeToString() for batch in batches]
+    
+    LOGGER.info("Submitting batches to the handlers")
+    
+    
+    for batch in post_batch_list:
+        try:
+            response = post_batch(batch)
+        except urllib.error.HTTPError as error:
+            LOGGER.info("Rest Api is not reachable")
+            response = json.loads(error.fp.read().decode('utf-8'))
+            LOGGER.info(response['error']['title'])
+            LOGGER.info(response['error']['message'])
+    
+    
+    for batch in expected_batches:
+        response = get_batch_statuses([batch])
+        status = response['data'][0]['status']
+          
+    batch_list = get_batches()
+    data['batch_ids'] = [batch['header_signature'] for batch in batch_list['data']]
+    data['batch_list'] = batch_list
+    transaction_list = get_transactions()
+    data['transaction_list'] = transaction_list
+    transaction_ids = [trans['header_signature'] for trans in transaction_list['data']]
+    data['transaction_ids'] = transaction_ids
+    block_list = get_blocks()
+    data['block_list'] = block_list
+    block_ids = [block['header_signature'] for block in block_list['data']]
+    data['block_ids'] = block_ids[:-1]
+    batch_ids = [block['header']['batch_ids'][0] for block in block_list['data']]
+    expected_head = block_ids[0]
+    data['expected_head'] = expected_head
+    state_addresses = [state['address'] for state in get_state_list()['data']]
+    data['state_address'] = state_addresses
+    state_head_list = [get_state_address(address)['head'] for address in state_addresses]
+    data['state_head'] = state_head_list
+    data['address'] = address
+    data['limit'] = LIMIT
+    data['start'] = expected_batches[::-1][0]
+    return data
                   
  
 def pytest_addoption(parser):
@@ -102,7 +211,6 @@ def pytest_addoption(parser):
 
    
 def pytest_collection_modifyitems(config, items):
-
     """Filters tests based on markers when parameters passed
        through the cli
     """
@@ -147,92 +255,3 @@ def pytest_collection_modifyitems(config, items):
         selected_items = items[:num]
         items[:] = selected_items
         return items
-
-@pytest.fixture(scope="session", autouse=True)
-def setup(request):
-    """Setup method for posting batches and returning the 
-       response
-    """
-    data = {}
-    signer = get_signer()
-    expected_trxns  = {}
-    expected_batches = []
-    transaction_list = []
-    initial_state_length = len(get_state_list())
-    initial_batch_length = batch_count()
-    initial_transaction_length = transaction_count()
-    address = _get_client_address()
-
-    LOGGER.info("Creating intkey transactions with set operations")
-    
-    txns = [create_intkey_transaction("set", [] , 50 , signer) for i in range(BATCH_SIZE)]
-
-    for txn in txns:
-        dict = MessageToDict(
-                txn,
-                including_default_value_fields=True,
-                preserving_proto_field_name=True)
-                
-        expected_trxns['trxn_id'] = [dict['header_signature']]
-        expected_trxns['payload'] = [dict['payload']]
-                    
-    LOGGER.info("Creating batches for transactions 1trn/batch")
-
-    batches = [create_batch([txn], signer) for txn in txns]
-
-    for batch in batches:
-        dict = MessageToDict(
-                batch,
-                including_default_value_fields=True,
-                preserving_proto_field_name=True)
-
-        batch_id = dict['header_signature']
-        expected_batches.append(batch_id)
-    
-    length_batches = len(expected_batches)
-    length_transactions = len(expected_trxns)
-        
-    data['expected_batch_length'] = initial_batch_length + length_batches
-    data['expected_trn_length'] = initial_transaction_length + length_transactions
-    data['expected_txns'] = expected_trxns['trxn_id'][::-1]
-    data['payload'] = expected_trxns['payload'][::-1]
-    data['expected_batches'] = expected_batches[::-1]
-    data['signer_key'] = signer.get_public_key().as_hex()
-    
-    post_batch_list = [BatchList(batches=[batch]).SerializeToString() for batch in batches]
-    
-    LOGGER.info("Submitting batches to the handlers")
-    
-    for batch in post_batch_list:
-        try:
-            response = post_batch(batch)
-        except urllib.error.HTTPError as error:
-            LOGGER.info("Rest Api is not reachable")
-            response = json.loads(error.fp.read().decode('utf-8'))
-            LOGGER.info(response['error']['title'])
-            LOGGER.info(response['error']['message'])
-    
-                
-    block_list = get_blocks()
-    data['block_list'] = block_list
-    batch_list = get_batches()
-    data['batch_list'] = batch_list
-    transaction_list = get_transactions()
-    data['transaction_list'] = transaction_list
-    transaction_ids = [trans['header_signature'] for trans in transaction_list['data']]
-    data['transaction_ids'] = transaction_ids
-    block_ids = [block['header_signature'] for block in block_list['data']]
-    data['block_ids'] = block_ids[:-1]
-    batch_ids = [block['header']['batch_ids'][0] for block in block_list['data']]
-    data['batch_ids'] = batch_ids
-    expected_head = block_ids[0]
-    data['expected_head'] = expected_head
-    state_addresses = [state['address'] for state in get_state_list()['data']]
-    data['state_address'] = state_addresses
-    state_head_list = [get_state_address(address)['head'] for address in state_addresses]
-    data['state_head'] = state_head_list
-    data['address'] = address
-    data['limit'] = LIMIT
-    data['start'] = expected_batches[::-1][0]
-    data['family_name']=[block['batches'][0]['transactions'][0]['header']['family_name'] for block in block_list['data']]
-    return data
